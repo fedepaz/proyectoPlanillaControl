a9d202975140f7bcfcf40c11bb2f86d5fbaee24f
FedericoPaz <=>
2024-06-24 15:06:13 -0300

    finalizado dataRoute.test, la idea es hacer todos los tests primero para comenzar con el frontEnd

--- backEnd/test/dataRoute.test.js
+++ backEnd/test/dataRoute.test.js
@@ -56,7 +56,7 @@ describe("GET /data/mediosTec", function () {
   it("should return status 200 and fetch mediosTec options", function (done) {
     request(app)
       .get("/data/mediosTec")
-      .end((err, res) => {
+      .end((_err, res) => {
         expect(res.status).to.equal(200);
         expect(res.body.mediosTec).to.be.an("array").that.is.not.empty;
         expect(res.body.mediosTec[0]).to.have.property("label", "Option 2");
@@ -83,10 +83,91 @@ describe("GET /data/tipoPro", function () {
   it("should return status 200 and fetch tipoPro options", function (done) {
     request(app)
       .get("/data/tipoPro")
-      .end((err, res) => {
+      .end((_err, res) => {
         expect(res.status).to.equal(200);
         expect(res.body.tipoPro).to.be.an("array").that.is.not.empty;
         expect(res.body.tipoPro[0]).to.have.property("label", "Option1");
+        done();
+      });
+  });
+});
+
+describe("GET /data/demora", function () {
+  let findStub;
+
+  before(function () {
+    findStub = sinon.stub(Demora, "find").returns({
+      select: sinon.stub().returns({
+        exec: sinon.stub().resolves([{ _id: "1", label: "Si" }]),
+      }),
+    });
+  });
+
+  after(function () {
+    findStub.restore();
+  });
+
+  it("should return status 200 and fetch tipoPro options", function (done) {
+    request(app)
+      .get("/data/demora")
+      .end((_err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.demora).to.be.an("array").that.is.not.empty;
+        expect(res.body.demora[0]).to.have.property("label", "Si");
+        done();
+      });
+  });
+});
+
+describe("GET /data/tipoVuelo", function () {
+  let findStub;
+
+  before(function () {
+    findStub = sinon.stub(TipoVuelo, "find").returns({
+      select: sinon.stub().returns({
+        exec: sinon.stub().resolves([{ _id: "1", label: "arribo" }]),
+      }),
+    });
+  });
+
+  after(function () {
+    findStub.restore();
+  });
+
+  it("should return status 200 and fetch tipoPro options", function (done) {
+    request(app)
+      .get("/data/tipoVuelo")
+      .end((_err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.tipoVuelo).to.be.an("array").that.is.not.empty;
+        expect(res.body.tipoVuelo[0]).to.have.property("label", "arribo");
+        done();
+      });
+  });
+});
+
+describe("GET /data/funcion", function () {
+  let findStub;
+
+  before(function () {
+    findStub = sinon.stub(Funcion, "find").returns({
+      select: sinon.stub().returns({
+        exec: sinon.stub().resolves([{ _id: "1", label: "supervisor" }]),
+      }),
+    });
+  });
+
+  after(function () {
+    findStub.restore();
+  });
+
+  it("should return status 200 and fetch tipoPro options", function (done) {
+    request(app)
+      .get("/data/funcion")
+      .end((_err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.funcion).to.be.an("array").that.is.not.empty;
+        expect(res.body.funcion[0]).to.have.property("label", "supervisor");
         done();
       });
   });
