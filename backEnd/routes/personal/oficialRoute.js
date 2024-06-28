e62c16f19afa4d46fc9ce01458356774101070d3
FedericoPaz <=>
2024-06-28 18:16:38 -0300

    cambiado a node-version:"20", por annotations de actions

--- backEnd/routes/personal/oficialRoute.js
+++ backEnd/routes/personal/oficialRoute.js
@@ -66,7 +66,7 @@ router.post("/", async (req, res) => {
         message: "Faltan datos de Oficial",
       });
     }
-
+    //validate!!!
     const newOficial = await Oficial.create({
       dni,
       firstname,
