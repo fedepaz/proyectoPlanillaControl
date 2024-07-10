27fee8e508b50feabdd7327af46a0920bb4ad301
FedericoPaz <=>
2024-07-10 14:39:04 -0300

    cambiado a helmet 2.3.0, sumado noCache, y varios...

--- backEnd/index.js
+++ backEnd/index.js
@@ -23,6 +23,10 @@ app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter({}));
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+var ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 app.use(express.json());
 app.use(cors());
