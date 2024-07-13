916db59291e7bce3ac13d40bdb885809d2fc3bc5
FedericoPaz <=>
2024-07-13 20:30:50 -0300

    regresado planillas schema, para pasar lint... todavía no voy a tocar el front...

--- frontEnd/src/planillas/components/Planillas.tsx
+++ frontEnd/src/planillas/components/Planillas.tsx
@@ -14,7 +14,7 @@ import {
   Typography,
 } from "@mui/material";
 import { useFieldArray, useFormContext, useWatch } from "react-hook-form";
-import { planillaSchema, defaultValues } from "../types/schema";
+import { Schema, defaultValues } from "../types/schema";
 import { RHFAutocomplete } from "../../components/RHFAutocomplete";
 import {
   useDemora,
